61bad4aa7caa86ec066ef3f4830f98461371cfb8
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-12-14 19:52:03 +0530

    done

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -98,7 +98,7 @@ const Hero = () => {
     <h2 className="lg:text-2xl text-xl flex items-center mt-4">
       <FontAwesomeIcon icon={faAngleDoubleRight} className="text-lg text-DarkYellow mx-2" />
       Air <FontAwesomeIcon icon={faAngleDoubleRight} className="text-lg text-DarkYellow mx-2" />
-      Maritime <FontAwesomeIcon icon={faAngleDoubleRight} className="text-lg text-DarkYellow mx-2" />
+      Sea <FontAwesomeIcon icon={faAngleDoubleRight} className="text-lg text-DarkYellow mx-2" />
       Road
     </h2>
   </div>
